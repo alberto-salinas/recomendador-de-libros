a0e6e30d7d2f618702e918e8e843f70b447384f1
jose.rodriguez-salinas <jose.rodriguez-salinas@workday.com>
2018-01-20 22:41:27 -0800

    add endpoint dataEntries; returns json formatted book data

--- panda-app.py
+++ panda-app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	return "Hola panda"
+
+@app.route("/dataEntries")
+def dataEntries():
+	data = json.load(open("data.json"))
+	return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(port=9000)
